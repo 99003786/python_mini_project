411c77f836ca6efc29eef578dff16388d1b384ff
99003786 <sangam.patel@ltts.com>
2021-03-22 09:07:55 +0530

    Updated

--- 3_Implementation/src/main.py
+++ 3_Implementation/src/main.py
@@ -1,49 +1,107 @@
-#using pandas library
+# using pandas library
 import pandas as pd
-#using openpyxl to write in master sheet
+# using openpyxl to write in master sheet
 from openpyxl import load_workbook
 
-xls = pd.ExcelFile('PythonExcelSheets.xlsx')
-df1 = pd.read_excel(xls, 'Sheet1')
-df2 = pd.read_excel(xls, 'Sheet2')
-df3 = pd.read_excel(xls, 'Sheet3')
-df4 = pd.read_excel(xls, 'Sheet4')
-df5 = pd.read_excel(xls, 'Sheet5')
-print("Type Print", "\n")
 
-df = pd.merge(df1, df2)
-df = pd.merge(df, df3)
-df = pd.merge(df, df4)
-df = pd.merge(df, df5)
-print(df, "\n")
+# Reading Excel File Using Pandas Library
+# ########################SANGAM Function Starts###################################################################################################################
+def sangam():
+    san = pd.ExcelFile('PythonExcelSheets.xlsx')
+    df1 = pd.read_excel(san, 'Sheet1')
+    df2 = pd.read_excel(san, 'Sheet2')
+    df3 = pd.read_excel(san, 'Sheet3')
+    df4 = pd.read_excel(san, 'Sheet4')
+    df5 = pd.read_excel(san, 'Sheet5')
+    ps = pd.DataFrame(df1, columns=['Ps No', 'Name'])
+    # print(ps)
+    # converting ps no data frame to a List
+    psnolist = ps['Ps No'].values.tolist()
+    # Here Making a Dictionary From Priviously Created Ps No List
+    dict1 = {"key1": psnolist}
+    # printing Dictionary so that user can pick anyone Unique key from dictionary
+    print(dict1)
+    print("Enter Unique Ps No Of Person")
+    # Taking User Input For Unique Key
+    ui = int(input())
+    # This Part Will Check Wether User Has Inputed valid Key Or not
+    key_to_lookup = 'key1'
+    # if key is valid it will Print (Unique Key Is True) This Message
+    if key_to_lookup in dict1:
+        print("Unique Key Is True")
+    # if key is valid it will Print (Please Enter Valid Unique Key of a Person) This Message
+    else:
+        print("Please Enter Valid Unique Key of a Person")
+    # Now Here Locking The found Column which user has inputed so it will fetch data from row only infront of column
+    df1.set_index("Ps No", inplace=True)
+    result1 = df1.loc[ui]
+    df2.set_index("Ps No", inplace=True)
+    result2 = df2.loc[ui]
+    df3.set_index("Ps No", inplace=True)
+    result3 = df3.loc[ui]
+    df4.set_index("Ps No", inplace=True)
+    result4 = df4.loc[ui]
+    df5.set_index("Ps No", inplace=True)
+    result5 = df5.loc[ui]
+    # creating a list to append data frames to a list
+    ls = []
+    lm = []
+    # appending the list using for loop
+    for i in range(len(result1)):
+        ls.append(result1[i])
+    lm.append(len(ls))
+    for i in range(len(result2)):
+        # appending the list but not repeating common items by this If Condition
+        if i >= 2:
+            ls.append(result2[i])
+    lm.append(len(ls) + 2)
+    for i in range(len(result3)):
+        if i >= 2:
+            ls.append(result3[i])
+    lm.append(len(ls) + 2)
+    for i in range(len(result4)):
+        if i >= 2:
+            ls.append(result4[i])
+    lm.append(len(ls) + 2)
+    for i in range(len(result5)):
+        if i >= 2:
+            ls.append(result5[i])
+    lm.append(len(ls) + 2)
+    # calculating length of complete list and assigning it to length variable
+    # length = (len(ls))
+    # print(length)
+    print(ls)
+    print(lm)
+    summary = pd.DataFrame(lm)
+    # opening Excel Workbook#######################################################################################################################################
+    # path = "PythonExcelSheets.xlsx"
+    ExcelWorkbook1 = load_workbook('PythonExcelSheets.xlsx')
+    if 'summarysheet' not in ExcelWorkbook1.sheetnames:
+        with pd.ExcelWriter('PythonExcelSheets.xlsx', engine="openpyxl", mode='a') as writer1:
+            summary.to_excel(writer1, sheet_name='summarysheet')
+    # return ls,lm
+    return ls
 
-path = r"PythonExcelSheets.xlsx"
-print("Enter Unique Id Ps No")
-x = int(input())
-df1 = pd.DataFrame(df, columns=["Ps No","Name", "Email", "Dept ID", "Module Name","Location","Domain", "Duration of Internship","Floor", "Stipend","Gender","Age","Phone","Education","Occupation","Experience (Years)","Salary","Company Name","Address","City","County","State","ZIP","Degree","Unnamed: 10","Invigilator Name","Subject","Invigilator  Mail ID",
-                               "Mobile numberof the Invigilator", "10th Marks", "12th Marks", "Virtual Room No", "Product","Discount Band", "Units Sold","Manufacturing Price","Sale Price","Gross Sales","Profit"])
-df1.set_index("Ps No",inplace=True)
-result = df1.loc[x]
-print(result)
 
-
+# #################################SANGAM Function Ends############################################################################################################
+# creating an Empty List
+lk = []
+print("How Much person Data you Want")
+z = int(input())
+for j in range(z):
+    lk.append(sangam())
+print(lk)
+# converting my final list to a Data Frame
+final = pd.DataFrame(lk)
+path = "PythonExcelSheets.xlsx"
 ExcelWorkbook = load_workbook('PythonExcelSheets.xlsx')
 writer = pd.ExcelWriter('PythonExcelSheets.xlsx', engine='openpyxl')
-
 writer.book = ExcelWorkbook
+# checking if master sheet is already prsent then it will not create the master sheet again
 if 'mastersheet' in ExcelWorkbook.sheetnames:
     pfd = ExcelWorkbook['mastersheet']
     ExcelWorkbook.remove(pfd)
-result.to_excel(writer, sheet_name='mastersheet')
-
+final.to_excel(writer, sheet_name='mastersheet')
 writer.save()
 writer.close()
-
-
-
-
-
-
-
-
-
+print(len(lk))
